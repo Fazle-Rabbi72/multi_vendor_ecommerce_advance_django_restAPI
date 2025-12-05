361a11111eb9f5d0f3dc50572323648f6f3ac9e4
Fazle-Rabbi72 <fazlerabbi0172291@gmail.com>
2025-12-06 03:25:21 +0600

    updated forget password

--- userauths/views.py
+++ userauths/views.py
@@ -10,11 +10,12 @@ from userauths.models import User,Profile
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated,AllowAny
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from userauths.serializer import RegisterSerializer,MyTokenObtainPairSerializer,UserSerializer,ProfileSerializer
 
 import shortuuid
-
+import random
 
 
 class MyTokenObtainPairView(TokenObtainPairView):
@@ -27,33 +28,38 @@ class RegisterView(generics.CreateAPIView):
     permission_classes=(AllowAny,)
     serializer_class=RegisterSerializer
 
-def generate_otp():
-    uuid_key=shortuuid.uuid()
-    unique_key=uuid_key[:6]
-    return unique_key
+def generate_numeric_otp(length=7):
+        # Generate a random 7-digit OTP
+        otp = ''.join([str(random.randint(0, 9)) for _ in range(length)])
+        return otp
     
 class PasswordRestEmailVerify(generics.RetrieveAPIView):
     permission_classes = (AllowAny,)
     serializer_class = UserSerializer
-    throttle_scope = 'password_reset'
+    # throttle_scope = 'password_reset'
 
     def get_object(self):
         email = self.kwargs['email']
         user = User.objects.get(email=email)
 
         if user:
-            user.otp = generate_otp()
+            user.otp = generate_numeric_otp()
+            uidb64 = user.pk
+            
+             # Generate a token and include it in the reset link sent via email
+            refresh = RefreshToken.for_user(user)
+            reset_token = str(refresh.access_token)
+
+            # Store the reset_token in the user model for later verification
+            user.reset_token = reset_token
             user.save()
 
-            uidb64 = user.pk
-            otp = user.otp
-
-            Link = f"http://localhost:5173/create-new-password?otp={otp}&uidb64={uidb64}"
+            Link = f"http://localhost:5173/create-new-password?otp={user.otp}&uidb64={uidb64}&reset_token={reset_token}"
             
 
            
             subject = "Password Reset Link"
-            message = f"Your OTP is: {otp}\n\nClick the link to reset your password:\n{Link}"
+            message = f"Click the link to reset your password:\n{Link}"
             email_from = settings.EMAIL_HOST_USER
             recipient = [email]
 
@@ -63,23 +69,29 @@ class PasswordRestEmailVerify(generics.RetrieveAPIView):
         return user
 
 class PasswordChangeView(generics.CreateAPIView):
-    permission_classes=(AllowAny,)
-    serializer_class=UserSerializer
-    throttle_scope = 'password_change'
+    permission_classes = (AllowAny,)
+    serializer_class = UserSerializer
+    
     def create(self, request, *args, **kwargs):
         payload = request.data
-        otp=payload['otp']
-        uidb64=payload['uidb64']
-        password=payload['password']
         
+        otp = payload['otp']
+        uidb64 = payload['uidb64']
+        reset_token = payload['reset_token']
+        password = payload['password']
+
+
         user = User.objects.get(id=uidb64, otp=otp)
         if user:
             user.set_password(password)
-            user.otp=""
+            user.otp = ""
+            user.reset_token = ""
             user.save()
-            return Response({"massage":"Password changed Successfully"},status=status.HTTP_201_CREATED)
+
+            
+            return Response( {"message": "Password Changed Successfully"}, status=status.HTTP_201_CREATED)
         else:
-            return Response({"massage":"User Does not exist"},status=status.HTTP_404_NOT_FOUND)
+            return Response( {"message": "An Error Occured"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 class ProfileView(generics.RetrieveAPIView):
     serializer_class=ProfileSerializer
